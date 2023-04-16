25db7edfaddf58a7d6dfd6d26be676e7f9c967e6
Mahmoud Samara <115866038+Ma7moudSamara@users.noreply.github.com>
2023-04-16 18:32:52 +0300

    Add files via upload

---
+++ ENCS3320 - Computer Networks/PROJECT1/projectfiles/Project#1/Part2/main.py
@@ -0,0 +1,29 @@
+import time
+from socket import *
+
+print("\n\t\t\t\t\t\t\t{Welcome to our program}")
+print("\n\t\t(Note that : you can enter an infinite websites names)")
+while True:
+
+  url = input("\nPlease Enter the url like this formula (www.WebsiteName.com) :")
+  NameOFServer = url
+  PortOfServer = 80
+
+  clinetSoket = socket(AF_INET, SOCK_STREAM) # Create a socket object
+
+  clinetSoket.connect((NameOFServer,PortOfServer)) # Connect the client
+  StartTime = time.time() #Time when send requst
+
+  clinetSoket.send("HEAD / HTTP/1.1 \r\n".encode()) # Send some data
+  clinetSoket.send(("Hostname:"+url+" \r\n\r\n").encode())
+
+  modifiedSentence = clinetSoket.recv(1024) # receive some data
+
+  EndTime = time.time() #Time when recive response
+
+  print("From server:", modifiedSentence.decode()) #Display the response
+
+  ElapsedTime = EndTime - StartTime  #Response time
+  print(f"Elapsed time = { ElapsedTime * 1000  } ms ") #Display the response time
+
+  clinetSoket.close()
